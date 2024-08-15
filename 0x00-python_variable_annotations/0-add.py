d54e53d280c2e64514e0984c1d736196b7cb4088
codinBabe <teboola2@gmail.com>
2024-08-15 13:36:21 +0100

    compeleted: task 0-5

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Type-annotated function add that takes a float a and a float b as arguments and returns their sum as a float.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Add two floats a and b and return their sum as a float.
+
+    Parameters:
+    a (float): First number
+    b (float): Second number
+
+    Returns:
+    float: Sum of a and b
+    """
+    return a + b
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""String representation of a float"""
+
+
+def to_str(n: float) -> str:
+    """Convert a float or integer to a string representation."""
+    return str(n)
