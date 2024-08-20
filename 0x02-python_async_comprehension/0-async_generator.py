d5f55d86e2c7cf2e9702e443791c496040c379fe
codinBabe <teboola2@gmail.com>
2024-08-20 16:36:42 +0100

    fix:requirement error

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -9,4 +9,4 @@ from typing import AsyncGenerator
 async def async_generator() -> AsyncGenerator[float, None]:
     for i in range(10):
         await asyncio.sleep(1)
-        yield random.random() * 10
+        yield random.uniform(0, 10)
