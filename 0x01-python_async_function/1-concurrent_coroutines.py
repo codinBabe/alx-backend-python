5734f603f348504d090a7436b0d19c9c205b01cd
codinBabe <teboola2@gmail.com>
2024-08-20 15:46:07 +0100

    compeleted: task 0-2

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,15 +2,14 @@
 """ Wait for n random delays """
 
 
-
+import asyncio
 from typing import List
 wait_random = __import__("0-basic_async_syntax").wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """ Wait for a random delay n times """
-    delays = []
-    for _ in range(n):
-        delay = await wait_random(max_delay)
-        delays.append(delay)
-    return delays
+    waits = await asyncio.gather(
+        *tuple(map(lambda _: wait_random(max_delay), range(n)))
+    )
+    return sorted(waits)
---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import asyncio
+import random
+from typing import AsyncGenerator
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    for i in range(10):
+        yield random.uniform(0, 10)
+        await asyncio.sleep(1)
