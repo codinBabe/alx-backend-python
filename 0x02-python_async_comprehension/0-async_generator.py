bbf827f113da4cee2004abf2693ba477e62cb8a1
codinBabe <teboola2@gmail.com>
2024-08-21 11:18:34 +0100

    fix:check fail in task 0-1

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
+"""Async Generator function that generates random numbers"""
 
 
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
-    for i in range(10):
+async def async_generator() -> Generator[float, None, None]:
+    """Async Generator function that generates random numbers"""
+    for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
+
--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Async comprehension"""
+"""Async comprehension that uses async_generator"""
 
 
 from typing import List
@@ -7,5 +7,9 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
-    """Async comprehension"""
+    """
+    Async comprehension generator function that 
+    returns a list of random numbers
+    between 0 and 10
+    """
     return [i async for i in async_generator()]
