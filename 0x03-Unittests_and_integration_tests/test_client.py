d5bdbbabeea028d9376d491dbdb4fa2f52ab0593
codinBabe <teboola2@gmail.com>
2024-09-16 17:38:35 +0100

    completed:task 4

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -19,3 +19,14 @@ class TestGithubOrgClient(unittest.TestCase):
         """Test that GithubOrgClient.org returns the correct value"""
         GithubOrgClient(org_name).org()
         mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
+
+    
+    @parameterized.expand([
+        ("google",),
+        ("abc",)
+    ])
+    @patch('client.get_json')
+    def test_public_repos_url(self, org_name, mock_get_json):
+        """Test that GithubOrgClient._public_repos_url returns the correct value"""
+        GithubOrgClient(org_name)._public_repos_url
+        mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
